b1cc081c39541dd1b4930191d8a23d988c1f855c
ygulcihan <ygulcihan@github.com>
2023-07-02 21:53:23 +0300

    touchkeyboard pi wip

--- src/TouchKeyboard.py
+++ src/TouchKeyboard.py
@@ -3,28 +3,28 @@ from time import sleep
 import numpy as np
 
 class Button():
-    def __init__(self, pos, text, size=[70, 70]):
+    def __init__(self, pos, text, size=[65, 65]):
         self.pos = pos
         self.size = size
         self.text = text
 
 class TouchKeyboard:
 
-    buttonList = []
-    hOffset = 20
-    vOffset = 20
+    buttons = []
+    hOffset = 5
+    vOffset = 5
 
     keys = [
         ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"],
         ["Q", "W", "E", "R", "T", "Y", "U", "I", "O", "P"],
-        ["A", "S", "D", "F", "G", "H", "J", "K", "L", ";"],
-        ["Z", "X", "C", "V", "B", "N", "M", ",", ".", "/"]
+        ["A", "S", "D", "F", "G", "H", "J", "K", "L", "<-"],
+        ["Z", "X", "C", "V", "B", "N", "M", ".", "Enter"]
         ]
 
     def __init__(self):
         for i in range(len(self.keys)):
             for j, key in enumerate(self.keys[i]):
-                self.buttonList.append(Button([80 * j + self.hOffset, 80 * i + self.vOffset], key))
+                self.buttons.append(Button([80 * j + self.hOffset, 80 * i + self.vOffset], key))
 
     def cornerRect(self, img, bbox, l=30, t=5, rt=1,
                colorR=(255, 0, 255), colorC=(0, 255, 0)):
@@ -40,8 +40,8 @@ class TouchKeyboard:
         """
         x, y, w, h = bbox
         x1, y1 = x + w, y + h
-        if rt != 0:
-            cv2.rectangle(img, bbox, colorR, rt)
+        #if rt != 0:
+            #cv2.rectangle(img, bbox, colorR, rt)
         # Top Left  x,y
         cv2.line(img, (x, y), (x + l, y), colorC, t)
         cv2.line(img, (x, y), (x, y + l), colorC, t)
@@ -57,22 +57,26 @@ class TouchKeyboard:
 
         return img
 
-    def drawAll(self, img):
-        for button in self.buttonList:
+    def getKeyboardImage(self, img):
+        for button in self.buttons:
             x, y = button.pos
-            w, h = button.size
-            self.cornerRect(img, (button.pos[0], button.pos[1], button.size[0], button.size[1]), l=20, rt=5, t=1)
-            cv2.rectangle(img, button.pos, (x + w, y + h), (0, 0, 0), cv2.FILLED)
+            #w, h = button.size
+            self.cornerRect(img, (button.pos[0], button.pos[1], button.size[0], button.size[1]), l=20, rt=5, t=5)
+            #cv2.rectangle(img, button.pos, (x + w, y + h), (0, 0, 0), cv2.FILLED)
             cv2.putText(img, button.text, (x + 20, y + 45),
-                        cv2.FONT_HERSHEY_PLAIN, 3, (255, 255, 255), 4)
+                        cv2.FONT_HERSHEY_PLAIN, 2, (255, 255, 255), 3)
             
         return img
+    
+    def clickEvent(self, event, x, y, flags, param):
+        i = 0
+    
     
 if __name__ == "__main__":
     import numpy as np
     tk = TouchKeyboard()
-    img = np.zeros((350, 830))
-    img = tk.drawAll(img)
+    img = np.zeros((320, 800))
+    img = tk.getKeyboardImage(img)
     cv2.imshow("Touch Keyboard", img)
     cv2.waitKey(0)
     
